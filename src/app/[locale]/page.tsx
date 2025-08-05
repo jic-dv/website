6f6f42e1b2002745cfe16f74547b3436d9375005
jic-dv <jic-dev@outlook.com>
2025-08-05 19:49:38 -0400

    add website upload v1.0.0

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,13 @@
+import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
+
+export default function Page() {
+  const t = useTranslations("HomePage");
+
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <Link href="/about">{t("about")}</Link>
+    </div>
+  );
+}
