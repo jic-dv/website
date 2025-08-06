98efe6b29be01c42fd4a93b190e89dad9ab87ad3
jic-dv <jic-dev@outlook.com>
2025-08-06 00:01:24 -0400

    fix: dynamic h2 section title size

--- src/components/section-title/index.tsx
+++ src/components/section-title/index.tsx
@@ -16,7 +16,7 @@ export default function SectionTitle({ title, subtitle, alignment = 'center' }: 
 
     return (
         <div className={cn('flex flex-col gap-2 mb-8', alignmentClasses[alignment])}>
-            <h2 className='text-3xl font-bold tracking-tight'>{title}</h2>
+            <h2 className='text-lg lg:text-3xl font-bold tracking-tight'>{title}</h2>
             {subtitle && (
                 <p className='text-muted-foreground max-w-prose text-sm sm:text-base'>{subtitle}</p>
             )}
